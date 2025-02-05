9bfe481fb56df090b95824f274cf42769a426f1a
Jens582 <jens.wieczorek@freenet.de>
2025-02-05 16:15:53 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,12 @@ from layouts.app_layout import layout_app
 from callbacks.controller_callbacks import register_callbacks
 from source.app_controller import AppController
 
-if __name__ == "__main__":
-    app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])    
-    app.layout = layout_app
-    app_controller = AppController()
-    register_callbacks(app_controller)
-    app.run_server(debug=True)
+
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server    
+app.layout = layout_app
+app_controller = AppController()
+register_callbacks(app_controller)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)  
